4f93cc701c1964f209a478c1738ece976df2f571
bowndim <apowell00@gmail.com>
2025-10-04 17:02:27 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,24 +11,6 @@ export default {
   components: {
     MainComponent,
   },
-  mounted() {
-    const params = new URLSearchParams(window.location.search);
-    const k = params.get('kanji') || params.get('q') || params.get('k');
-    if (!k) return;
-
-    // If you use v-model="search":
-    if (this.search !== undefined) {
-      this.search = k;
-    }
-
-    // Or target a plain input:
-    const box = document.querySelector('#filter');
-    if (box) {
-      box.value = k;
-      box.dispatchEvent(new Event('input', { bubbles: true }));
-      box.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true }));
-    }
-  }
 };
 </script>
 
